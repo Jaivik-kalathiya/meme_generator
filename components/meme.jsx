f282542620181692ee4eb4e27dfc0d035f68c0b5
Jaivik Kalathiya <jaivikkalathiya@gmail.com>
2024-02-13 23:19:51 +0530

    TEXT Field is Remaining

---
+++ components/meme.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import memesData from "../data/memeData";
+
+export default function Meme() {
+  // const [memeImage, setMemeImage] = React.useState("");
+
+  // function getMemeImage() {
+  //   const memesArray = memesData.data.memes;
+  //   const randomNumber = Math.floor(Math.random() * memesArray.length);
+  //   setMemeImage(memesArray[randomNumber].url);
+   
+  // }
+const [meme,setmeme] = React.useState({
+  uptext:"",
+  bottomtext:"",
+  imgurl:"",
+})
+
+
+  function getMemeImage(){
+   const memesArray = memesData.data.memes;
+   const randomNumber = Math.floor(Math.random() * memesArray.length);
+      setmeme(prev=>{
+        return{
+          ...prev,
+          imgurl:memesArray[randomNumber].url
+        }
+      })
+  }
+
+  
+  return (
+    <>
+      <main>
+        <form className="form">
+          <div>
+            <label className="form--label">Top text</label>
+            <input type="text" className="form--input" placeholder="top Text" />
+          </div>
+
+          <div>
+            <label className="form--label">Bottom text</label>
+            <input
+              type="text"
+              className="form--input"
+              placeholder="Bottom Text"
+            />
+          </div>
+          <button className="form--button" onClick={getMemeImage} type="button">
+            Get a new meme image 🖼
+          </button>
+        </form>
+        <img src={meme.imgurl} className="meme--image" />
+      </main>
+    
+      
+    </>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from '../components/Header'
+import Meme from '../components/meme'
+
+
+ export default function App() {
+  
+  return (
+    <>
+  <Header/>
+  <Meme/>
+    </>
+  )
+}
+
+
